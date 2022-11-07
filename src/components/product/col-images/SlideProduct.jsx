c968d6bc4343ef61b561562e769b54e8e5fe44cb
Mariano Bozzoletti <mbozzoletti@hotmail.com>
2022-11-07 01:47:43 -0300

    creando y estilando el modal

--- src/components/product/col-images/SlideProduct.jsx
+++ src/components/product/col-images/SlideProduct.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import NextIcon from "@/components/icons/NextIcon";
 import PrevIcon from "@/components/icons/PrevIcon";
+import { useRef } from "react";
 
-const SlideProduct = ({ ARRAY_IMGS = [], ARRAY_IMGS_SMALL = [] }) => {
+const SlideProduct = ({
+  ARRAY_IMGS = [],
+  ARRAY_IMGS_SMALL = [],
+  isOpenModal = false,
+  handleCloseModal = null,
+  handleOpenModal = () => {},
+  ...props
+}) => {
   const [index, setIndex] = useState(0);
+
+  const btnSlider = useRef(null);
+
+  useEffect(() => {
+    isOpenModal && btnSlider.current.classList.remove("md:hidden");
+  }, [isOpenModal]);
 
   const handleClickNext = () => {
     index === ARRAY_IMGS.length - 1 ? setIndex(0) : setIndex(index + 1);
@@ -12,24 +26,35 @@ const SlideProduct = ({ ARRAY_IMGS = [], ARRAY_IMGS_SMALL = [] }) => {
   const handleClickPrev = () => {
     index === 0 ? setIndex(ARRAY_IMGS.length - 1) : setIndex(index - 1);
   };
+
   return (
-    <section className="grid md:grid-cols-4 md:gap-4">
+    <section className="grid md:grid-cols-4 md:gap-4" {...props}>
+      {isOpenModal && (
+        <button onClick={handleCloseModal} className="text-right md:col-span-4">
+          cerrar
+        </button>
+      )}
+
       <div className="relative col-span-4">
         <img
           src={ARRAY_IMGS[index]}
           alt="product image"
-          className="aspect-[16/14] w-full md:aspect-[16/16] md:rounded-md"
+          className="aspect-[16/14] w-full md:aspect-[16/16] md:cursor-pointer md:rounded-md"
+          onClick={handleOpenModal}
         />
-        <div className="absolute left-0 top-1/2 flex w-full -translate-y-1/2 justify-between px-4">
+        <div
+          ref={btnSlider}
+          className="absolute left-0 top-1/2 flex w-full -translate-y-1/2 justify-between px-4 md:hidden"
+        >
           <button
             onClick={handleClickPrev}
-            className="flex h-8 w-8 items-center justify-center rounded-full bg-white md:hidden"
+            className="flex h-8 w-8 items-center justify-center rounded-full bg-white"
           >
             <PrevIcon />
           </button>
           <button
             onClick={handleClickNext}
-            className="flex h-8 w-8 items-center justify-center rounded-full bg-white md:hidden"
+            className="flex h-8 w-8 items-center justify-center rounded-full bg-white"
           >
             <NextIcon />
           </button>
